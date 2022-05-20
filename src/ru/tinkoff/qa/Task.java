a0bbd586626ed731a52b9508d54dbb1a813291a2
a.n.shemonaev <a.n.shemonaev@tinkoff.ru>
2022-05-20 17:11:21 +0300

    first commit

---
+++ src/ru/tinkoff/qa/Task.java
@@ -0,0 +1,9 @@
+package ru.tinkoff.qa;
+
+
+public class Task {
+    public static void main(String[] args) {
+        String name = "Иван Иванов";
+        System.out.println("Привет! Я - " + name);
+    }
+}
