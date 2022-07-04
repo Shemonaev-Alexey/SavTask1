9e6354b1f03eb64c7a6908a98fb742694c26fbae
Sb05 <shcolnik2010@yandex.ru>
2022-07-04 22:24:12 +0300

    add new task 2

--- src/ru/tinkoff/qa/Task.java
+++ src/ru/tinkoff/qa/Task.java
@@ -1,12 +1,37 @@
 package ru.tinkoff.qa;
 
 
+import java.util.Scanner;
+
 public class Task {
+
     /**
      * Дать пользователю ввести букву, есть буква есть в исходном слове, вывести, что молодецэ
      * Иначе, что не молодец
      */
     public static void main(String[] args) {
         String line = "Интуиция";
+        Scanner bykovka1 = new Scanner(System.in);
+        System.out.println("Введите буковку");
+        String byk = bykovka1.next();
+        char bykovk = byk.charAt(0);
+        char[] bykovki = line.toCharArray();
+        boolean isExistLetter = isExistLetter(bykovk, bykovki);
+        if (isExistLetter) {
+            System.out.println("Маладес");
+        } else {
+            System.out.println("Не маладес");
+        }
+
+    }
+
+    private static boolean isExistLetter(char bykovk, char[] bykovki) {
+        boolean isExistLetter = false;
+        for (char abc : bykovki) {
+            if (bykovk == abc) {
+                isExistLetter = true;
+            }
+        }
+        return isExistLetter;
     }
 }
